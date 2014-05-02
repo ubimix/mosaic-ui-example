04d7e3ddb26a877992653c282f5a2402ffef848c
kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-05-02 22:04:17 +0200

    Delete buttons were added

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ $(function() {
         listSelector : '#list',
         datalayersSelector : '#map [data-map-layer]'
     }
-
+    Mosaic.DataSet.registerResourceStates([ {
+        field : '_hello',
+        on : 'helloResource',
+        off : 'byeResource',
+        priority : 10
+    } ]);
     var application = new Mosaic.AppConfigurator(appConfig);
     application.start();
 });
